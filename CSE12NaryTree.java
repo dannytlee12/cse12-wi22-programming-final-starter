8f4575ccf7b381109c5d0a085837106d7a770a6d
Danny Lee <dtl001@ucsd.edu>
2022-03-13 01:53:46 -0800

    added methods and fixed bugs

--- CSE12NaryTree.java
+++ CSE12NaryTree.java
@@ -3,7 +3,7 @@
  * Name:
  * ID:
  * Email:
- * File description: 
+ * File description:
  */
 
 import java.util.List;
@@ -16,7 +16,7 @@ import java.util.Queue;
  * TODO: Add class header
  */
 public class CSE12NaryTree<E extends Comparable<E>> {
-    
+
     /**
      * This inner class encapsulates the data and children for a Node.
      * Do NOT edit this inner class.
@@ -24,20 +24,20 @@ public class CSE12NaryTree<E extends Comparable<E>> {
     protected class Node{
         E data;
         List<Node> children;
-    
+
         /**
          * Initializes the node with the data passed in
-         * 
+         *
          * @param data The data to initialize the node with
          */
         public Node(E data) {
             this.data = data;
             this.children = new ArrayList<>();
         }
-    
+
         /**
          * Getter for data
-         * 
+         *
          * @return Return a reference to data
          */
         public E getData() {
@@ -46,7 +46,7 @@ public class CSE12NaryTree<E extends Comparable<E>> {
 
         /**
          * Setter for the data
-         * 
+         *
          * @param data Data that this node is set to
          */
         public void setData(E data) {
@@ -55,7 +55,7 @@ public class CSE12NaryTree<E extends Comparable<E>> {
 
         /**
          * Getter for children
-         * 
+         *
          * @return reference to the list of children
          */
         public List<Node> getChildren() {
@@ -64,7 +64,7 @@ public class CSE12NaryTree<E extends Comparable<E>> {
 
         /**
          * Returns the number of children
-         * 
+         *
          * @return number of children
          */
         public int getNumChildren() {
@@ -74,22 +74,22 @@ public class CSE12NaryTree<E extends Comparable<E>> {
 
         /**
          * Add the given node to this node's list of children
-         * 
+         *
          * @param node The node to add
          */
         public void addChild(Node node) {
             children.add(node);
         }
-    
+
     }
-    
+
     Node root;
     int size;
     int N;
 
     /**
      * Constructor that initializes an empty N-ary tree, with the given N
-     * 
+     *
      * @param N The N the N-tree should be initialized with
      */
     public CSE12NaryTree(int N) {
@@ -105,22 +105,87 @@ public class CSE12NaryTree<E extends Comparable<E>> {
      * TODO: Add method header
      */
     public void add(E element) {
-        //TODO
+      if(element == null){
+        throw new NullPointerException();
+      }
+
+      Node newNode = new Node(element);
+      if(root == null){
+        root = newNode;
+        size++;
+        return;
+      }
+      //index of the parent of the node to be added
+      //root is index 0
+      int parentIdx = (size-1)/N;
+      ArrayList<Integer> ancestors = new ArrayList<Integer>();
+      while(parentIdx > 0){
+        //ancestors holds the order of children to traverse to get to the next
+        //insertion point
+        ancestors.add(0,(parentIdx-1)%N);
+        parentIdx = (parentIdx)/N;
+      }
+      Node curr = root;
+      //how many times to go down before getting to the parent of the new node
+      int parentLevel = ancestors.size();
+      for(int i = 0; i < parentLevel; i++){
+        curr = curr.getChildren().get(ancestors.get(i));
+      }
+      curr.addChild(newNode);
+      size++;
     }
 
     /**
      * TODO: Add method header
      */
     public boolean contains(E element) {
-        //TODO
-        return false;
+      if(element == null){
+        throw new NullPointerException();
+      }
+
+      boolean found = false;
+        if(root.getData() == element){
+          return true;
+        }
+        if(root.getNumChildren() == 0){
+          return false;
+        }
+        for(int i = 0; i < root.getNumChildren(); i++){
+          //if any children are element, return true
+          CSE12NaryTree childTree = new CSE12NaryTree(N);
+          childTree.root = root.getChildren().get(i);
+          found = found || childTree.contains(element);
+        }
+        return found;
     }
 
     /**
      * TODO: Add method header
      */
     public ArrayList<E> sortTree(){
-        //TODO
-        return null;
+        ArrayList<E> ansList = new ArrayList<E>();
+        ArrayList<Node> pQ = new ArrayList<Node>(size);
+        PriorityQueue<E> pQ2 = new PriorityQueue<E>(size);
+        if(root == null){
+          return ansList;
+        }
+        pQ.add(root);
+        while(pQ.size() != 0){
+          int n = pQ.size();
+          while(n > 0){
+            Node curr = pQ.get(0);
+            pQ.remove(0);
+            pQ2.add(curr.getData());
+
+            for(int i = 0; i < curr.getNumChildren(); i++){
+              pQ.add(curr.getChildren().get(i));
+            }
+            n--;
+          }
+        }
+        while(pQ2.size() != 0){
+          ansList.add(pQ2.remove());
+        }
+        return ansList;
     }
 }
--- CSE12NaryTreeTester.java
+++ CSE12NaryTreeTester.java
@@ -3,9 +3,9 @@
  * Name:
  * ID:
  * Email:
- * File description: 
+ * File description:
  */
- 
+
 import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.*;
@@ -14,6 +14,33 @@ import java.util.*;
  * TODO: Add class header
  */
 public class CSE12NaryTreeTester {
+    CSE12NaryTree<Integer> tree = new CSE12NaryTree<Integer>(3);
+    CSE12NaryTree<Integer> tree2 = new CSE12NaryTree<Integer>(5);
+
+    @Before
+    public void setUp(){
+      tree.add((Integer)0);
+      tree.add((Integer)1);
+      tree.add((Integer)2);
+      tree.add((Integer)3);
+      tree.add((Integer)4);
+      tree.add((Integer)5);
+      tree.add((Integer)6);
+      tree2.add((Integer)3);
+      tree2.add((Integer)2);
+      tree2.add((Integer)0);
+      tree2.add((Integer)8);
+      tree2.add((Integer)5);
+      tree2.add((Integer)4);
+      tree2.add((Integer)7);
+      tree2.add((Integer)6);
+      tree2.add((Integer)1);
+      tree2.add((Integer)9);
+      tree2.add((Integer)12);
+      tree2.add((Integer)11);
+      tree2.add((Integer)10);
+      tree2.add((Integer)13);
+    }
     /**
      * TODO: Add test case description.
      */
@@ -22,12 +49,28 @@ public class CSE12NaryTreeTester {
 
     }
 
+
+    @Test
+    public void testAdd2(){
+      tree.add(7);
+      assertEquals((Integer)1, tree.root.getChildren().get(0).getData());
+      assertEquals((Integer)7, tree.root.getChildren().get(1).getChildren().get(0).getData());
+    }
+
     /**
      * TODO: Add test case description
      */
     @Test
     public void testContains(){
-        
+      assertTrue(tree.contains(0));
+      assertTrue(tree.contains(1));
+      assertTrue(tree.contains(2));
+      assertTrue(tree.contains(3));
+      assertTrue(tree.contains(4));
+      assertTrue(tree.contains(5));
+      assertTrue(tree.contains(6));
+      assertFalse(tree.contains(10));
+
     }
 
     /**
@@ -35,7 +78,11 @@ public class CSE12NaryTreeTester {
      */
     @Test
     public void testSortTree(){
-        
+      ArrayList<Integer> ansList = tree2.sortTree();
+      assertEquals((Integer)0, ansList.get(0));
+      for(int i = 0; i < tree2.size; i++){
+        assertEquals((Integer)i, ansList.get(i));
+      }
     }
 
     /**
@@ -43,6 +90,6 @@ public class CSE12NaryTreeTester {
      */
     @Test
     public void testCustom(){
-        
+
     }
 }
